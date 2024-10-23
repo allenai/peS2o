cc4800ffd798bb9baa29552059b294073cf91e90
Luca Soldaini <luca@soldaini.net>
2024-10-22 21:06:18 -0700

    Soldni/2024 refresh (#8)
    * porting scripts
    
    * requirements
    
    * fixed imports
    
    * fixes
    
    * updated lang id
    
    * tweak
    
    * better pbar
    
    * better queue
    
    * git commit typo
    
    * debug all
    
    * kwargs
    
    * wip
    
    * fixing s2ag processing
    
    * added final step
    
    * new
    
    * refining
    
    * malformed fix
    
    * fixed query

---
+++ recipe/3_load_as_table/s2orc.sql
@@ -0,0 +1,26 @@
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `temp_lucas`.`llm_s2orc_v0-fos-license-2024-10-06` (
+    id STRING,
+    source STRING,
+    text STRING,
+    version STRING,
+    added STRING,
+    created STRING,
+    metadata STRUCT<
+        year:INT,
+        title:STRING,
+        abstract:STRING,
+        sha1:STRING,
+        fields_of_study:ARRAY<STRING>,
+        paragraphs:ARRAY<STRUCT<language:STRING,perplexity:DOUBLE,text:STRING>>,
+        count:INT,
+        top_frequencies:ARRAY<STRUCT<token:STRING,count:INT>>,
+        oa_license: STRING,
+        oa_url: STRING,
+        oa_status: STRING,
+        pdf_src: STRING,
+        pdf_hash: STRING
+    >
+)
+ROW FORMAT serde 'org.apache.hive.hcatalog.data.JsonSerDe'
+LOCATION 's3://ai2-llm/pretraining-data/sources/s2/v0-fos-license/documents/2024-10-06/dataset=s2orc'
